35f3e488ff187e9ca7fba207baf10c2a5ad0f413
ItsDixie <hipster.yt89@mail.ru>
2023-05-09 21:05:08 +0300

    Update

--- src/deep_learning.py
+++ src/deep_learning.py
@@ -55,20 +55,21 @@ class chessAI:
         print('---TRAINING...---')
         for i in range(num_games):
             while not board.is_game_over():
-                try:
-                    if(board.turn == chess.BLACK): # training AI to be the BEST BLACK AHAH
-                        next_state = self.get_board_state(board)
-                        state = next_state
-                        move = self.get_move(board)
-                        board.push(move)
-                        score = self.engine.analyse(board, chess.engine.Limit(time=6.0))["score"].relative.score()
-                        reward = score / 100.0
-                        action = list(board.move_stack)[-1].uci()
-                        self.update_q_table(state, action, reward, next_state)
-                        print(board)
-                        print(f'score {reward} of this move')
-                    else:
-                        board.push(self.engine.play(board, chess.engine.Limit(time=4.0)).move ) # Rybka engine moving WHITE figures and training mine AI
+                try: 
+                    next_state = self.get_board_state(board)
+                    state = next_state
+                    move = self.get_move(board)
+                    board.push(move)
+                    try:
+                        score = self.engine.analyse(board, chess.engine.Limit(time=3.0))["score"].relative.score()
+                    except Exception:
+                        score = self.engine.analyse(board, chess.engine.Limit(time=10.0))["score"].relative.score()
+                    reward = score / 100.0
+                    action = list(board.move_stack)[-1].uci()
+                    self.update_q_table(state, action, reward, next_state)
+                    print(board)
+                    print(f'score {reward} of this move')
+
                 except Exception as e:
                     self.export_table()
                     print('---TRAINING FAIL RESTART---')
